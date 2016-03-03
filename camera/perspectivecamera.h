0665e7502faf50eccfb42ae46a60cd7aa06ff1c3
Walter Kuppens <reshurum@gmail.com>
2016-03-03 14:18:12 -0800

    Documented the PerspectiveCamera class

--- camera/perspectivecamera.h
+++ camera/perspectivecamera.h
@@ -11,25 +11,33 @@ extern "C" {
 
 class PerspectiveCamera {
   public:
+    // The camera has both the view and projection matrices.
     glm::mat4 view;
     glm::mat4 projection;
 
+    // Used to initialize the view matrix.
     glm::vec3 position;
     glm::vec3 rotation;
     glm::vec3 front;
     glm::vec3 up;
 
+    // Used to initialize the projection matrix.
     GLfloat fov;
     GLfloat aspect;
     GLfloat near;
     GLfloat far;
 
+    // Initialize a camera with the defaults (Looking negative z).
     PerspectiveCamera();
+
+    // Create a camera using all params necessary to generate a perspective.
     PerspectiveCamera(const glm::vec3 position, const glm::vec3 rotation,
       const GLfloat fov, const GLfloat aspect, const GLfloat near,
       const GLfloat far);
 
-    // Update the view matrix and the perspective matrix.
+    // Update the view matrix and the perspective matrix with the current
+    // position and front values. In addition, front is also calculated from the
+    // rotation vector.
     void update();
 };
 
