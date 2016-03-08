3904aba47546048f5c74d7fc36d307806e90ad05
Walter Kuppens <reshurum@gmail.com>
2016-03-07 17:06:54 -0800

    Greyscale is very noir

--- framebuffers/glsl/post_frag.glsl
+++ framebuffers/glsl/post_frag.glsl
@@ -7,8 +7,6 @@ uniform sampler2D frameTexture;
 
 void main() {
   vec4 sample = texture(frameTexture, fragUv);
-  float average = (sample.x + sample.y + sample.z) / 3;
-
-  vec3 inversion = vec3(1.0f - sample);
-  color = vec4(inversion * average, 1.0f);
+  float average = 0.2126 * sample.r + 0.7152 * sample.g + 0.0722 * sample.b;
+  color = vec4(vec3(average), 1.0f);
 }
